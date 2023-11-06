68747ac56ed632b346dc8c0924a044a94eea6fc3
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-11-06 10:42:04 +0100

    Overall structure of the project

---
+++ src/main/java/edu/laplateforme/overlookhotel/OverlookhotelApplication.java
@@ -0,0 +1,13 @@
+package edu.laplateforme.overlookhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlookhotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverlookhotelApplication.class, args);
+    }
+
+}
