9731e3304ac6f2b01ed1fb18ad49fe493446ce56
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-11-07 15:12:33 +0100

    [FIX] refactor

--- launchFrontend.sh
+++ launchFrontend.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Vérifiez si Node.js est installé
+# Check if node is installed
 if ! [ -x "$(command -v node)" ]; then
   echo 'Error: Node.js is not installed. Please install Node.js and try again.' >&2
   exit 1
 fi
 
-# Vérifiez si npm est installé
+# Check if npm is installed
 if ! [ -x "$(command -v npm)" ]; then
   echo 'Error: npm is not installed. Please install npm and try again.' >&2
   exit 1
 fi
 
-# Installez Angular CLI s'il n'est pas déjà installé
+# Install Angular CLI if not already installed
 if ! [ -x "$(command -v ng)" ]; then
   echo 'Installing Angular CLI...'
   npm install -g @angular/cli
 fi
 
-# Allez dans le dossier frontend et lancez ng serve
+# Go to frontend folder and run ng serve
 cd frontend
 ng serve
 ```
